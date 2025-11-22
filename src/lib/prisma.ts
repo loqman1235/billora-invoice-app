804004c4f7b28a3defda0839f29e880b859352d2
loqman1235 <tonyvito12@gmail.com>
2025-11-22 08:49:38 +0100

    fix: remove ws import from prisma.ts and only load it in development

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { Pool, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import ws from "ws";
 
-neonConfig.webSocketConstructor = ws;
 neonConfig.poolQueryViaFetch = true;
+
+if (process.env.NODE_ENV !== "production") {
+  import("ws").then((ws) => {
+    neonConfig.webSocketConstructor = ws.default;
+  });
+}
+
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
